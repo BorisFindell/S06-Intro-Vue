8c6d5378481d7dea15b93fa95f17cdcb9b92748d
Martin Frangul <borisfindell@Air-de-Martin.home>
2022-02-01 17:03:45 +0100

    ultim ex

--- s6-intro-vue/src/components/Botons.vue
+++ s6-intro-vue/src/components/Botons.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container d-flex justify-content-center w-100">
-    <button type="button" class="button ant btn-primary d-flex justify-content-center" @click="$emit('ant-sentence')">Anterior</button>
-    <button type="button" class="button seg btn-warning d-flex justify-content-center" @click="$emit('seg-sentence')">Siguiente</button>
+    <button type="button" class="button ant mt-5 btn-primary d-flex justify-content-center" @click="$emit('ant-sentence')">Anterior</button>
+    <button type="button" class="button seg mt-5 btn-warning d-flex justify-content-center" @click="$emit('seg-sentence')">Siguiente</button>
   </div>
 </template>
 
@@ -21,5 +21,6 @@ export default {
   width: 30%;
   padding: 10px;
 }
+
 
 </style>
--- s6-intro-vue/src/components/Home.vue
+++ s6-intro-vue/src/components/Home.vue
@@ -15,7 +15,6 @@ import Botons from './Botons.vue'
 import Benving from './Benving.vue'
 
 
-// import Escena from '@/components/Escena.vue'
 
 export default {
   name: 'home',
@@ -23,13 +22,13 @@ export default {
   data () {
     return {
       elementos: [
-  {txt: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial", img:("../img/1.jpg")},
+  {txt: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial", img:require("../assets/1.jpg")},
 
-  {txt: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", img: ("../img/2.jpg")},
+  {txt: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", img:require("../assets/2.jpg")},
 
-  {txt: "L'heroi va decidir travessar la porta que el portava a casa", img: ("../img/3.jpg")},
+  {txt: "L'heroi va decidir travessar la porta que el portava a casa", img:require("../assets/3.jpg")},
 
-  {txt: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...", img: ("../img/4.jpg")}
+  {txt: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...", img:require("../assets/4.jpg")}
       ],
       currentSentence: 0
     }
@@ -54,7 +53,10 @@ export default {
 
 </script>
 
-<style>
-
-
+<style scoped>
+    #home{
+        height:100vh;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
 </style>
